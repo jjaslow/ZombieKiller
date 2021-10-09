38a93970e2b8c6af4963d67b3698977a234b2cdd
Jason Jaslow <jason@jaslow.com>
2021-10-09 12:40:58 -0400

    Final bug tweaks. More fog variability and features.

--- Assets/Scripts/TextureOffset.cs
+++ Assets/Scripts/TextureOffset.cs
@@ -9,17 +9,37 @@ public class TextureOffset : MonoBehaviour
     [SerializeField] float scrollSpeed = .5f;
     Renderer rend;
 
+    [SerializeField] Texture2D[] textures;
+
+    int direction1;
+    int direction2;
+
     void Start()
     {
         rend = GetComponent<Renderer>();
+
+        int index = Random.Range(0, textures.Length);
+        rend.material.SetTexture("_MainTex", textures[index]);
+
+        int dirIndex = Random.Range(0, 2);
+        direction1 = dirIndex == 0 ? -1 : 1;
+        dirIndex = Random.Range(0, 2);
+        direction2 = dirIndex == 0 ? -1 : 1;
+
+        float offset = Mathf.Sin(Random.Range(0, 1f));      //scrollSpeed;
+        float offsetY = Mathf.Cos(Random.Range(0, 1f));      //scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, offsetY));
     }
 
     void Update()
     {
-        float offset = Mathf.Sin(Time.time * scrollSpeed);      //scrollSpeed;
-        float offsetY = Mathf.Cos(Time.time * scrollSpeed);      //scrollSpeed;
+        float offset = Mathf.Sin(Time.time * scrollSpeed * direction1);      //scrollSpeed;
+        float offsetY = Mathf.Cos(Time.time * scrollSpeed * direction2);      //scrollSpeed;
         rend.material.SetTextureOffset("_MainTex", new Vector2(offset, offsetY));
     }
+
+
+
 }
 
 
